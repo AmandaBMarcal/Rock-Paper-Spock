536197881b9b94ec3e9b783fc7e67d549f84c40b
AmandaBMarcal <amandabmarcal@gmail.com>
2019-07-23 11:44:18 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// document.querySelector("button").addEventListener('click', palindrome)
+// function palindrome (){
+// let str = document.getElementsByTagName("input")[0].value.split(' ').join('')
+// fetch(`/api?palindrome=${str}`)
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     document.querySelector("p").innerHTML = data.palindrome
+//   });
+// }
+//
+//
+document.querySelector("button").addEventListener("click",()=>{
+// Computer choice
+const randomChoice = ()=> {
+let choice = Math.ceil(Math.random()*5);
+  if (choice === 1){
+    choice "Rock"
+  }else if (choice ===2){
+    choice "Paper"
+  }else if (choice ===3){
+    choice "Scissor"
+  }else if (choice ===4){
+    choice "Spock"
+  }else (choice ===5){
+    choice "Lizard"
+  }
+    return choice
+  }
+
+
+  })
+
+
+  function win {
+    if ("rock" > "paper"){
+  }
+    return "rock"
+  }
